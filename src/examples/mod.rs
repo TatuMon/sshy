703b60a64eb360ae0aec619086e300522c89f2a8
Juan Cruz Montiel <juan.montiel@paians.com>
2024-06-01 00:28:03 -0300

    Started to work on the JSON Editor example

--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -1,2 +1,3 @@
 pub mod hello_world;
 pub mod app_counter;
+pub mod json_editor;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use color_eyre::eyre::Result;
+// use color_eyre::eyre::Result;
+
+use std::error::Error;
 
 pub mod examples;
 
-fn main() -> Result<()> {
-    // sshy::examples::hello_world::run()
-    examples::app_counter::run()
+//fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
+    // examples::hello_world::run()
+    // examples::app_counter::run()
+    examples::json_editor::run()
 }
