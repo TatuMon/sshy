4819ccda8cea37d93fb7984d63320ae81bedfee1
tatumon <tatumonar@proton.me>
2024-11-03 19:52:33 +0000

    tokio added

--- src/commands/ssh_keygen.rs
+++ src/commands/ssh_keygen.rs
@@ -1,11 +1,9 @@
 use core::panic;
-use std::process::{self, Command, Stdio};
+use std::process::{Command, Stdio};
 
-use color_eyre::eyre::{eyre, Context, Result};
+use color_eyre::eyre::{Context, Result};
 
-use crate::{
-    events::messages::Message, model::sections_state::public_keys_list_state::NewPublicKeyState,
-};
+use crate::model::sections_state::public_keys_list_state::NewPublicKeyState;
 
 use super::Task;
 
@@ -30,26 +28,32 @@ pub struct SshKeygenCmd {
     comment: String,
     running: bool,
     task_msg_tx: Option<super::TaskMessageTx>,
-    cmd_handle: Option<std::process::Child>
+    cmd_handle: Option<std::process::Child>,
 }
 
 impl Task for SshKeygenCmd {
     fn start(&mut self, task_msg_tx: super::TaskMessageTx) -> Result<()> {
-        let args: [&str;6] = [
-            "-t", self.keytype.into(),
-            "-f", &self.filename,
-            "-C", &self.comment
-        ];        
+        let args: [&str; 6] = [
+            "-t",
+            self.keytype.into(),
+            "-f",
+            &self.filename,
+            "-C",
+            &self.comment,
+        ];
 
         let cmd_handle = Command::new("ssh-keygen")
             .args(args)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
-            .spawn().wrap_err("error spawning ssh-keygen")?;
+            .spawn()
+            .wrap_err("error spawning ssh-keygen")?;
 
         self.cmd_handle = Some(cmd_handle);
         self.task_msg_tx = Some(task_msg_tx);
         self.running = true;
+
+        tokio::spawn(self.handle());
 
         Ok(())
     }
@@ -62,7 +66,7 @@ impl Task for SshKeygenCmd {
     fn terminate(&mut self) -> Result<()> {
         match &mut self.cmd_handle {
             None => panic!("no running command"),
-            Some(handle) => handle.kill().wrap_err("failed to terminate command")
+            Some(handle) => handle.kill().wrap_err("failed to terminate command"),
         }
     }
 }
@@ -76,7 +80,11 @@ impl SshKeygenCmd {
             passphrase: None,
             running: false,
             task_msg_tx: None,
-            cmd_handle: None
+            cmd_handle: None,
         }
+    }
+
+    async fn handle(&self) {
+        println!("{}", self.comment);
     }
 }
