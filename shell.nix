5e947c27160b562054b385b0e45ab289f5be8f86
tatumon <tatumonar@proton.me>
2025-01-20 12:48:30 -0300

    nix: nix-shell now installs the stable release of the rust toolchain
    Now, when using nix-shell, the project is compiled using the stable
    release of rustc

--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-    pkgs.mkShell {
-        packages = with pkgs;[
-            rustup
-            mdbook
-        ];
-    }
+{ pkgs ? import <nixpkgs> { } }:
+let
+  overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustup
+  ];
+  RUSTC_VERSION = overrides.toolchain.channel;
+  shellHook = ''
+    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+  '';
+}
